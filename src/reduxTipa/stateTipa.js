839b59275d2c9b416d9d01e46d06b2622d59abe2
reddicka <yu.nailka.rav@yandex.ru>
2022-11-15 22:28:47 +0500

    perenos store

--- src/reduxTipa/stateTipa.js
+++ src/reduxTipa/stateTipa.js
@@ -2,85 +2,164 @@ let rerenderEntireTree = () => {
     console.log('rerender')
 }
 
-let stateTipa = {
-    profilePageData: {
-        posts: [
-            {
-                id: 1,
-                text: 'Пост про письки',
-                likes: 12
-            },
-            {
-                id: 2,
-                text: 'Пост про сиськи',
-                likes: 15
-            },
-        ],
-        newPostText: ''
-    },
-    dialogsPageData: {
-        dialogs: [
-            {
-                id: 1,
-                name: 'Стасян'
-            },
-            {
-                id: 2,
-                name: 'Василка'
-            },
-            {
-                id: 3,
-                name: 'Данилбка'
-            },
-            {
-                id: 4,
-                name: 'Танюха'
+let store = {
+    stateTipa {
+        profilePageData: {
+            posts: [
+                {
+                    id: 1,
+                    text: 'Пост про письки',
+                    likes: 12
+                },
+                {
+                    id: 2,
+                    text: 'Пост про сиськи',
+                    likes: 15
+                },
+            ],
+            newPostText: '',
+            addPost() {
+                const newPostText = stateTipa.profilePageData.newPostText
+
+                if (newPostText !== '') {
+                    let newPost = {
+                        id: stateTipa.profilePageData.posts.length + 1,
+                        text: newPostText,
+                        likes: 0
+                    }
+
+                    stateTipa.profilePageData.posts.push(newPost)
+                    stateTipa.profilePageData.newPostText = ''
+
+                    rerenderEntireTree(stateTipa)
+                }
             }
-        ],
-        messages: [
-            {
-                id: 1,
-                message: 'Вам песьмо такое',
-                type: 'inbox'
-            },
-            {
-                id: 2,
-                message: 'А я ему такое',
-                type: 'outbox'
-            },
-            {
-                id: 3,
-                message: 'Привет',
-                type: 'inbox'
-            },
-            {
-                id: 4,
-                message: 'Привет писька',
-                type: 'outbox'
-            }
-        ],
-        newMessageText: ''
-    }
-}
-
-window.state = stateTipa
-
-export const addPost = () => {
-    const newPostText = stateTipa.profilePageData.newPostText
-
-    if (newPostText !== '') {
-        let newPost = {
-            id: stateTipa.profilePageData.posts.length + 1,
-            text: newPostText,
-            likes: 0
+        },
+        dialogsPageData: {
+            dialogs: [
+                {
+                    id: 1,
+                    name: 'Стасян'
+                },
+                {
+                    id: 2,
+                    name: 'Василка'
+                },
+                {
+                    id: 3,
+                    name: 'Данилбка'
+                },
+                {
+                    id: 4,
+                    name: 'Танюха'
+                }
+            ],
+            messages: [
+                {
+                    id: 1,
+                    message: 'Вам песьмо такое',
+                    type: 'inbox'
+                },
+                {
+                    id: 2,
+                    message: 'А я ему такое',
+                    type: 'outbox'
+                },
+                {
+                    id: 3,
+                    message: 'Привет',
+                    type: 'inbox'
+                },
+                {
+                    id: 4,
+                    message: 'Привет писька',
+                    type: 'outbox'
+                }
+            ],
+            newMessageText: ''
         }
-
-        stateTipa.profilePageData.posts.push(newPost)
-        stateTipa.profilePageData.newPostText = ''
-
-        rerenderEntireTree(stateTipa)
-    }
+    },
 }
+
+// let stateTipa = {
+//     profilePageData: {
+//         posts: [
+//             {
+//                 id: 1,
+//                 text: 'Пост про письки',
+//                 likes: 12
+//             },
+//             {
+//                 id: 2,
+//                 text: 'Пост про сиськи',
+//                 likes: 15
+//             },
+//         ],
+//         newPostText: ''
+//     },
+//     dialogsPageData: {
+//         dialogs: [
+//             {
+//                 id: 1,
+//                 name: 'Стасян'
+//             },
+//             {
+//                 id: 2,
+//                 name: 'Василка'
+//             },
+//             {
+//                 id: 3,
+//                 name: 'Данилбка'
+//             },
+//             {
+//                 id: 4,
+//                 name: 'Танюха'
+//             }
+//         ],
+//         messages: [
+//             {
+//                 id: 1,
+//                 message: 'Вам песьмо такое',
+//                 type: 'inbox'
+//             },
+//             {
+//                 id: 2,
+//                 message: 'А я ему такое',
+//                 type: 'outbox'
+//             },
+//             {
+//                 id: 3,
+//                 message: 'Привет',
+//                 type: 'inbox'
+//             },
+//             {
+//                 id: 4,
+//                 message: 'Привет писька',
+//                 type: 'outbox'
+//             }
+//         ],
+//         newMessageText: ''
+//     }
+// }
+
+window.state = store.stateTipa
+
+// export const addPost = () => {
+//     const newPostText = stateTipa.profilePageData.newPostText
+//
+//     if (newPostText !== '') {
+//         let newPost = {
+//             id: stateTipa.profilePageData.posts.length + 1,
+//             text: newPostText,
+//             likes: 0
+//         }
+//
+//         stateTipa.profilePageData.posts.push(newPost)
+//         stateTipa.profilePageData.newPostText = ''
+//
+//         rerenderEntireTree(stateTipa)
+//     }
+// }
 
 export const updateNewPostText = (newText) => {
     stateTipa.profilePageData.newPostText = newText
