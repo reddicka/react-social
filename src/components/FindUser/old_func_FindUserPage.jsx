0c08b2ded38a05fe3cd09c87904ae31d2d3bc0bb
reddicka <yu.nailka.rav@yandex.ru>
2022-11-26 08:16:43 +0500

    компонента FindUserPage теперь классовая

--- src/components/FindUser/old_func_FindUserPage.jsx
+++ src/components/FindUser/old_func_FindUserPage.jsx
@@ -1,12 +1,40 @@
-import React from "react";
 import style from './FindUserPage.module.css'
 import User from "./User/User";
 import axios from "axios";
+import {Component} from "react";
 
-class FindUserPage extends React.Component {
-    constructor(props) {
-        super(props)
+// const FindUserPage = (props) => {
+//     if (props.users.length === 0) {
+//         axios
+//             .get("https://social-network.samuraijs.com/api/1.0/users")
+//             .then(response => {
+//                 props.setUsers(response.data.items)
+//             })
+//     }
+//
+//     const usersList = props.users.map(user =>
+//         <User
+//             key={user.id}
+//             id={user.id}
+//             name={user.name}
+//             followed={user.followed}
+//             photos={user.photos}
+//             location={user.location}
+//             status={user.status}
+//             follow={props.follow}
+//             unfollow={props.unfollow}
+//         />
+//     )
+//
+//     return (
+//         <ul className={style.users_list}>
+//             { usersList }
+//         </ul>
+//     )
+// }
 
+class FindUserPage extends Component {
+    render() {
         if (this.props.users.length === 0) {
             axios
                 .get("https://social-network.samuraijs.com/api/1.0/users")
@@ -14,9 +42,7 @@ class FindUserPage extends React.Component {
                     this.props.setUsers(response.data.items)
                 })
         }
-    }
 
-    render() {
         const usersList = this.props.users.map(user =>
             <User
                 key={user.id}
