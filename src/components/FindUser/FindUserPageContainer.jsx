b554b89e91d81be978fa779fe43061f840d0027b
reddicka <yu.nailka.rav@yandex.ru>
2022-11-28 19:30:28 +0500

    fix clear function

--- src/components/FindUser/FindUserPageContainer.jsx
+++ src/components/FindUser/FindUserPageContainer.jsx
@@ -7,7 +7,44 @@ import {
     setUsersAC,
     unfollowAC
 } from "../../redux/find_users-reducer";
+import React from "react";
+import axios from "axios";
 
+class FindUserPageContainer extends React.Component {
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios
+                .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`)
+                .then(response => {
+                    this.props.setUsers(response.data.items)
+                    this.props.setTotalUsers(response.data.totalCount)
+                })
+        }
+    }
+
+    onSetPageClick = (page) => {
+        this.props.setCurrentPage(page)
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${page}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return (
+            <FindUserPage
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+                totalUsers={this.props.totalUsers}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onSetPageClick={this.onSetPageClick}
+            />
+        )
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -43,4 +80,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(FindUserPage)
+export default connect(mapStateToProps, mapDispatchToProps)(FindUserPageContainer)
