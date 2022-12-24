3da9430d29a3072a90c344169ca483a521a68dd5
reddicka <yu.nailka.rav@yandex.ru>
2022-12-24 07:08:15 +0500

    add extension Redux DevTools

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, legacy_createStore as createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import findUsersReducer from "./find_users-reducer";
@@ -16,8 +16,12 @@ let reducers = combineReducers({
     form: formReducer
 })
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware))
+// настройки для браузерного расширения Redux DevTools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)))
+// let store = createStore(reducers, applyMiddleware(thunkMiddleware))
 
-window.store = store
+// временная штука
+window.__store__ = store
 
 export default store
